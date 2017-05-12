29bbd13f4c7546f1862dbd522c0b6407d3c5ffef
tchvu3 <tchvu3@gmail.com>
2017-05-12 13:05:47 +0300

    added missing files

--- lev-isha/app/src/main/java/com/hadassah/azrieli/lev_isha/utility/Checklist.java
+++ lev-isha/app/src/main/java/com/hadassah/azrieli/lev_isha/utility/Checklist.java
@@ -1,29 +1,52 @@
 package com.hadassah.azrieli.lev_isha.utility;
 
+import android.content.Context;
+import android.content.res.Resources;
+
 import com.hadassah.azrieli.lev_isha.R;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  * Created by Avihu Harush on 06/05/2017
  * E-Mail: tchvu3@gmail.com
  */
 
-public class Checklist {
+public class Checklist implements Serializable {
 
-    /*
-    * This class relevancy is still under investigation
-    * */
+    private static final long serialVersionUID = 1L;
 
-    private static final int[] rKeys = {
-            R.string.doctor_name, R.string.address, R.string.appointment_date, R.string.appointment_hour,
-            R.string.remember_to_bring_with_you, R.string.references, R.string.prescription,
-            R.string.Previous_diagnoses, R.string.tests_results, R.string.subjects_i_want_to_talk_about,
-            R.string.new_symptoms, R.string.additional_information, R.string.changes_in_life,
-            R.string.personal_medical_history, R.string.family_health_background, R.string.drugs_i_take,
-            R.string.additional_questions_to_the_doctor, R.string.summary_of_the_doctors_Remarks,
-            R.string.determine_your_risk_of_heart_disease, R.string.ask_about_future_treatment,
-            R.string.should_you_come_back_for_further_inspections, R.string.have_you_got_answers_for_these_questions,
-            R.string.what_is_my_medical_condition, R.string.what_should_i_do_to_address_my_problem,
-            R.string.why_should_i_do_so, R.string.if_not_ask_again
-    };
+    private static Checklist instance;
+    private static final String FILE_NAME = "checklist_entries.data";
+    private static final boolean DEBUG_DELETE_FILE = false; //KEEP THIS FALSE!
+
+    public static Checklist getInstance(Context context) {
+        if(instance != null)
+            return instance;
+        Resources res = context.getResources();
+        File profileObject = new File(context.getFilesDir().getAbsolutePath()+"//"+FILE_NAME);
+        if(DEBUG_DELETE_FILE)
+            profileObject.delete();
+        if(profileObject.exists()) {
+            try {
+                ObjectInputStream ois = new ObjectInputStream(new FileInputStream(profileObject));
+                instance = (Checklist)ois.readObject();
+                ois.close();
+                return instance;
+            } catch(Exception exp){exp.printStackTrace();}
+        }
+        instance = new Checklist();
+        return instance;
+    }
+
+    private Checklist() {
+
+    }
+
+
 
 }
